f8f819adc86414c27010a690602ff7f40ef8570c
Ben Dolly <bromeodolly@gmail.com>
2020-12-20 22:49:35 -0500

    🚧 adds GET_invictus_post
    simple handler to grab an invicuts post

--- handler.py
+++ handler.py
@@ -1,18 +1,30 @@
+import os
 import json
+import requests
+# from bs4 import BeautifulSoup
+
+invictus_api = os.environ['INVICTUS_WEIGHTLIFTING_API']
+
+def GET_invictus_post(user_name, password, posts=1):
+    """GET Invicitus Weightlifting WP blog post"""
+
+    api_req = requests.get(
+        invictus_api+"&per_page="+str(posts), 
+        auth=(user_name,password)
+    )
+
+    return api_req.json()
+    
 
 
 def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+    response = json.dumps(
+                    GET_invictus_post(os.environ['INVICTUS_USER'], os.environ['INVICTUS_PASS'])
+                )
 
     return response
+    
 
     # Use this code if you don't use the http event with the LAMBDA-PROXY
     # integration
@@ -22,3 +34,8 @@ def hello(event, context):
         "event": event
     }
     """
+
+
+    
+    
+
