4fe54bd8f798ecaec8a82fb874c0e61e1fc1bede
Ben Dolly <bromeodolly@gmail.com>
2021-01-02 00:24:32 -0500

    ✨ adds stateMachine definiton
    state machine to pull post and dump to staging bucket

--- handler.py
+++ handler.py
@@ -6,27 +6,31 @@ import boto3
 s3_resource = boto3.resource('s3')
 invictus_api = os.environ['INVICTUS_WEIGHTLIFTING_API']
 
-def GET_invictus_post(user_name, password, posts=1):
+
+def GET_invictus_post(event, context):
     """GET Invicitus Weightlifting WP blog post"""
 
     api_req = requests.get(
-        invictus_api+"&per_page="+str(posts), 
-        auth=(user_name,password)
+        invictus_api+"&per_page="+str(1),
+        auth=(os.environ['INVICTUS_USER'], os.environ['INVICTUS_PASS'])
     )
 
     return api_req.json()
-    
 
 
-def dump_post_to_bucket(event, context):
+def dump_post_to_bucket(invictus_raw_posts, context):
 
-    response = GET_invictus_post(os.environ['INVICTUS_USER'], os.environ['INVICTUS_PASS'])[0]
-    
-    workout_slug = response["slug"]
+    post = invictus_raw_posts[0]
 
-    s3object = s3_resource.Object(os.environ['INVICTUS_BUCKET'], '{slug}/_raw_{slug}.json'.format(slug=workout_slug))
-    
-    return s3object.put(
-        Body=(bytes(json.dumps(response).encode('UTF-8')))
+    bucket_path = '{slug}/_raw_{slug}.json'.format(slug=post["slug"])
+
+    print('- Creating bucket path:  {}'.format(bucket_path))
+
+    s3object = s3_resource.Object(os.environ['INVICTUS_BUCKET'], bucket_path)
+    print('- Dumping post "{}" to bucket'.format(post["title"]["rendered"]))
+
+    s3object_success = s3object.put(
+        Body=(bytes(json.dumps(post).encode('UTF-8')))
     )
-    
+
+    return json.dumps(post)
