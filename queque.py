c252cd1b1a693d23a0424b471313fc755388a4c5
Ben Dolly <bromeodolly@gmail.com>
2022-02-27 14:47:57 -0500

    push items to sqs and setup lambda step func trigger for worker

--- queque.py
+++ queque.py
@@ -2,23 +2,13 @@ import boto3
 import os
 import json
 import requests
-from handler import GET_invictus_post, dump_post_to_bucket
+import math
 
 invictus_api_endpoint = os.environ['INVICTUS_WEIGHTLIFTING_API']
 invicuts_api_requests_sqs_name = os.environ['INVICTUS_WEIGHTLIFTING_API_QUEQUE_NAME']
 
 _sqs_client = None
-_s3_client = None
 _sqs_url = None
-_cloudwatch_client = None
-_event_rule_name = 'invictus-weightlifting-de-ProcessUnderscorequequeU-M5RUJJ9J5Q0C'
-
-
-def get_cloudwatch_events_client():
-    global _cloudwatch_client
-    if _cloudwatch_client is None:
-        _cloudwatch_client = boto3.client('events')
-    return _cloudwatch_client
 
 
 def get_sqs_client():
@@ -39,6 +29,7 @@ def get_queue_url():
 
 
 def calc_sqs_items_from_api(event, context):
+
     posts_per_page = event.get('posts_per_page', False) or 1
     page_num = event.get('page', False) or 1
 
@@ -48,16 +39,12 @@ def calc_sqs_items_from_api(event, context):
         auth=(os.environ['INVICTUS_USER'], os.environ['INVICTUS_PASS'])
     )
 
-    # total number of wp api resutls pages
-    total_pages = round(
-        int(api_req.headers["X-WP-Total"])/posts_per_page)
-
     return [{
         "page": idx+1,
         "posts_per_page": posts_per_page
     } for idx, val in
         enumerate(
-        [None] * total_pages
+        [None] * int(api_req.headers["x-wp-totalpages"])
     )]
 
 
@@ -65,51 +52,15 @@ def push_item_to_queque(event, ctx):
     client = get_sqs_client()
     sqs_url = get_queue_url()
 
+    # batch by pages of 100 messages per minute
+    batchPage = math.ceil(int(event["page"])/100)
+    delaySecs = batchPage * 60
+
+    event["delaySecs"] = delaySecs
+    event["batchPage"] = batchPage
+
     return client.send_message(
         QueueUrl=sqs_url,
-        MessageBody=json.dumps(event))
-
-
-# TODO: split this up into multipe lambas to make as part of step functions
-
-def process_queque_items(event, ctx):
-    sqs = get_sqs_client()
-    sqs_url = get_queue_url()
-    events = get_cloudwatch_events_client()
-    events.enable_rule(Name=_event_rule_name)
-
-    # grab single sqs item
-    response = sqs.receive_message(
-        QueueUrl=sqs_url,
-        MaxNumberOfMessages=1,
+        DelaySeconds=delaySecs,
+        MessageBody=json.dumps(event),
     )
-
-    messages = response.get('Messages')
-
-    if not messages:
-        events.disable_rule(Name=_event_rule_name)
-        return "No Messages found in {}".format(sqs_url)
-
-    receipt = messages[0].get('ReceiptHandle')
-
-    request_params = json.loads(
-        messages[0].get('Body', {})
-    )
-
-    posts = GET_invictus_post(request_params, {})
-
-    processed = []
-
-    for post in posts:
-        if "_links" in post:
-            del post["_links"]
-
-        success = dump_post_to_bucket(post, {})
-
-        if success:
-            processed.append(success["title"]["rendered"])
-
-    if(len(processed) == len(posts)):
-        sqs.delete_message(QueueUrl=sqs_url, ReceiptHandle=receipt)
-
-    return processed
